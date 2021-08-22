aa676eaddae28f380134dccdb9c75a017b0d3f6e
Aoi <hyuugaaoi0511@gmail.com>
2021-08-22 22:16:29 +0900

    user admin 追加

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Admin;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
 
+        $user = new User();
+        $user
+            ->setUsername('user')
+            ->setPassword($this->passwordHasher->hashPassword($user, 'user'));
+        $manager->persist($user);
+        $admin = new Admin();
+        $admin
+            ->setUsername('admin')
+            ->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $manager->persist($admin);
         $manager->flush();
     }
 }
